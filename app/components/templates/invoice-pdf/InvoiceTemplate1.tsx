0905feabf81841d507702bc0fb5fcf01482893f5
sulphurninja <sulphurninja@gmail.com>
2024-01-01 23:02:56 +0530

    Fuck off

--- app/components/templates/invoice-pdf/InvoiceTemplate1.tsx
+++ app/components/templates/invoice-pdf/InvoiceTemplate1.tsx
@@ -19,8 +19,8 @@ const InvoiceTemplate = (data: InvoiceType) => {
 
     // Calculate net weight using the same logic as in BillFromSection
     const calculateNetWeight = () => {
-        const grossWeight = parseFloat(sender.address || 0);
-        const tareWeight = parseFloat(sender.zipCode || 0);
+        const grossWeight = parseFloat(sender?.address || 0);
+        const tareWeight = parseFloat(sender?.zipCode || 0);
         const netWeight = grossWeight - tareWeight;
         return isNaN(netWeight) ? 0 : netWeight;
     };
@@ -29,12 +29,12 @@ const InvoiceTemplate = (data: InvoiceType) => {
         <InvoiceLayout data={data}>
             <div className="flex justify-between">
                 <div>
-                    {details.invoiceLogo && (
+                    {details?.invoiceLogo && (
                         <img
-                            src={details.invoiceLogo}
+                            src={details?.invoiceLogo}
                             width={140}
                             height={100}
-                            alt={`Logo of ${sender.name}`}
+                            alt={`Logo of ${sender?.name}`}
                         />
                     )}
                     {/* <h1 className="mt-2 text-lg md:text-xl font-semibold text-blue-600">
@@ -60,9 +60,9 @@ const InvoiceTemplate = (data: InvoiceType) => {
                 <div>
                     <h3 className="text-lg font-semibold text-gray-800">
                     <dd className="col-span-3 text-gray-500">
-                {details.Name ? details.Name : <span style={{ visibility: 'hidden' }}>{"\u200B"}</span>}
+                {details?.Name ? details.Name : <span style={{ visibility: 'hidden' }}>{"\u200B"}</span>}
             </dd>
-                        <span>{details.vehicleNumber}</span>
+                        <span>{details?.vehicleNumber}</span>
                     </h3>
 
                 </div>
@@ -72,23 +72,23 @@ const InvoiceTemplate = (data: InvoiceType) => {
 
                             <dt className="col-span-2  -mt-3 font-semibold text-gray-800">
                                 <h3 className="text-lg  font-semibold text-gray-800">
-                                    <span className="">{details.invoiceNumber ? details.invoiceNumber : <span style={{ visibility: 'hidden' }}>{"\u200B"}</span>}</span>
-                                    <span className="block">{details.rstNumber? details.rstNumber : <span style={{ visibility: 'hidden' }}>{"\u200B"}</span>}</span>
+                                    <span className="">{details?.invoiceNumber ? details.invoiceNumber : <span style={{ visibility: 'hidden' }}>{"\u200B"}</span>}</span>
+                                    <span className="block">{details?.rstNumber? details.rstNumber : <span style={{ visibility: 'hidden' }}>{"\u200B"}</span>}</span>
                                 </h3>
                             </dt>
                             <dd className="col-span-3 mt-12 ml-56 absolute text-gray-500">
-                                {details.time1}
+                                {details?.time1}
                             </dd>
                             <dt className="col-span-3 mt-20 ml-56 absolute text-gray-500">
-                                {details.time2}
+                                {details?.time2}
                             </dt>
                         </dl>
                         <dl className="grid -mt-2  ml-6 sm:grid-cols-6 gap-x-3">
                             <dt className="col-span-2 absolute font-semibold text-gray-800">
-                                {details.date1}
+                                {details?.date1}
                             </dt>
                             <dt className=" absolute mt-8  col-span-2 font-semibold text-gray-800">
-                                {details.date2}
+                                {details?.date2}
                             </dt>
                         </dl>
                     </div>
@@ -96,9 +96,9 @@ const InvoiceTemplate = (data: InvoiceType) => {
             </div>
             <div className=" space-y-2">
                 <address className=" ml-20  not-italic text-gray-800">
-                    <span className='text-xl font-bold'>{sender.address} kg</span>
+                    <span className='text-xl font-bold'>{sender?.address} kg</span>
                     <br />
-                    <span className='text-xl font-bold'>{sender.zipCode} kg</span>
+                    <span className='text-xl font-bold'>{sender?.zipCode} kg</span>
                     <br />
                     <span className='text-xl font-bold'>{calculateNetWeight()} kg</span>
                     <br />
@@ -107,7 +107,7 @@ const InvoiceTemplate = (data: InvoiceType) => {
                     <dl className="grid sm:grid-cols-5 gap-x-3">
 
                         <dd className="col-span-2 mt-2 text-gray-500">
-                            {details.receivedRs}
+                            {details?.receivedRs}
 
                         </dd>
                     </dl>
@@ -129,21 +129,21 @@ const InvoiceTemplate = (data: InvoiceType) => {
                         src={details.signature.data}
                         width={120}
                         height={60}
-                        alt={`Signature of ${sender.name}`}
+                        alt={`Signature of ${sender?.name}`}
                     />
                 </div>
-            ) : details.signature?.data ? (
+            ) : details?.signature?.data ? (
                 <div className="mt-6">
                     <p className="text-gray-800">Signature:</p>
                     <p
                         style={{
                             fontSize: 30,
                             fontWeight: 400,
-                            fontFamily: `${details.signature.fontFamily}, cursive`,
+                            fontFamily: `${details?.signature.fontFamily}, cursive`,
                             color: "black",
                         }}
                     >
-                        {details.signature.data}
+                        {details?.signature.data}
                     </p>
                 </div>
             ) : null}
